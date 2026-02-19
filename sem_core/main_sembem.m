e9ea07e416c4a4d71edc4726498353a3ac9e456f
Erden Yildizdag <yildizdag@berkeley.edu>
2026-02-19 13:34:09 +0300

    SEM-BEM ongoing

--- sem_core/main_sembem.m
+++ sem_core/main_sembem.m
@@ -1,6 +1,6 @@
 %==========================================================================
 % Project       : TÜBİTAK 3501 (125M858)
-% Method        : Chebyshev Spectral Element Method (SEM)
+% Method        : Chebyshev Spectral Element - Boundary Element Method (SEM)
 % Meshing       : NURBS-Based Coarse-Quad Meshing
 %
 % Description   :
@@ -40,60 +40,60 @@ end
 %----------------
 tic;
 %
-Nurbs2D = iga2Dmesh(FileName,numPatch,local_dof);
+Nurbs2D = iga2Dmesh(FileName,numPatch,1);
 toc;
 %
 tic;
-sem2D = sem2Dmesh(Nurbs2D,N,local_dof);
-sem2D.ET = ET;
-sem2D.N = N;
-sem2D.non = size(sem2D.nodes,1);
-sem2D.dof = sem2D.non*local_dof;
-sem2D.E = E;
-sem2D.nu = nu;
-sem2D.rho = rho;
-sem2D.t = t;
-sem2D.D = (E*t^3)/12/(1-nu^2);
-sem2D.G = E/2/(1+nu);
-sem2D.Ds = (5/6)*sem2D.G*t;
-sem2D.lame = 2*sem2D.G/(1-sem2D.nu);  
+sembem2D = sembem2Dmesh(Nurbs2D,N,semPatch,bemPatch,shell_dof,fluid_dof);
+sembem2D.ET = ET;
+sembem2D.N = N;
+sembem2D.non = size(sembem2D.nodes,1);
+sembem2D.dof = sembem2D.non*local_dof;
+sembem2D.E = E;
+sembem2D.nu = nu;
+sembem2D.rho = rho;
+sembem2D.t = t;
+sembem2D.D = (E*t^3)/12/(1-nu^2);
+sembem2D.G = E/2/(1+nu);
+sembem2D.Ds = (5/6)*sembem2D.G*t;
+sembem2D.lame = 2*sembem2D.G/(1-sembem2D.nu);  
 %
 toc;
 tic;
-%----------
-% Solution
-%----------
-[K,M] = global2D(sem2D);
-toc;
-%-Boundary Conditions:
-tic;
-x_max = max(sem2D.nodes(:,1));
-x_min = min(sem2D.nodes(:,1));
-y_max = max(sem2D.nodes(:,2));
-y_min = min(sem2D.nodes(:,2));
-ind = find(sem2D.nodes(:,1)<x_min+1E-3|sem2D.nodes(:,1)>x_max-1E-3|sem2D.nodes(:,2)<y_min+1E-3|sem2D.nodes(:,2)>y_max-1E-3);
-BounNodes = unique([6.*ind-5; 6.*ind-4; 6.*ind-3; 6.*ind-2; 6.*ind-1; 6.*ind]);
-% BounNodes = unique([3.*ind-2; 3.*ind-1; 3.*ind]);
-K(BounNodes,:) = []; K(:,BounNodes) = [];
-M(BounNodes,:) = []; M(:,BounNodes) = [];
-toc;
-tic;
-%-Eigenvalue Solver
-sigma = 2000;
-[V,freq] = eigs(K,M,modeNum,sigma);
-[freq,loc] = sort((sqrt(diag(freq)-sigma)));
-toc;
-V = V(:,loc);
-freqHz = freq/2/pi;
-%
-all_nodes = 1:sem2D.dof;
-active = setdiff(all_nodes,BounNodes);
-uModes = zeros(sem2D.dof,modeNum);
-uModes(active,1:modeNum) = uModes(active,1:modeNum) + V(:,1:modeNum);
-sem2D.uModes = uModes;
-sem2D.freq = freq;
-sem2D.freqHz = freqHz;
-%-----------------
-% Post-Processing
-%-----------------
-% plotModeShapes(sem2D,modeNumPlot);
+% % %----------
+% % % Solution
+% % %----------
+% % [K,M] = global2D(sembem2D);
+% % toc;
+% % %-Boundary Conditions:
+% % tic;
+% % x_max = max(sembem2D.nodes(:,1));
+% % x_min = min(sembem2D.nodes(:,1));
+% % y_max = max(sembem2D.nodes(:,2));
+% % y_min = min(sembem2D.nodes(:,2));
+% % ind = find(sembem2D.nodes(:,1)<x_min+1E-3|sembem2D.nodes(:,1)>x_max-1E-3|sembem2D.nodes(:,2)<y_min+1E-3|sembem2D.nodes(:,2)>y_max-1E-3);
+% % BounNodes = unique([6.*ind-5; 6.*ind-4; 6.*ind-3; 6.*ind-2; 6.*ind-1; 6.*ind]);
+% % % BounNodes = unique([3.*ind-2; 3.*ind-1; 3.*ind]);
+% % K(BounNodes,:) = []; K(:,BounNodes) = [];
+% % M(BounNodes,:) = []; M(:,BounNodes) = [];
+% % toc;
+% % tic;
+% % %-Eigenvalue Solver
+% % sigma = 2000;
+% % [V,freq] = eigs(K,M,modeNum,sigma);
+% % [freq,loc] = sort((sqrt(diag(freq)-sigma)));
+% % toc;
+% % V = V(:,loc);
+% % freqHz = freq/2/pi;
+% % %
+% % all_nodes = 1:sembem2D.dof;
+% % active = setdiff(all_nodes,BounNodes);
+% % uModes = zeros(sembem2D.dof,modeNum);
+% % uModes(active,1:modeNum) = uModes(active,1:modeNum) + V(:,1:modeNum);
+% % sembem2D.uModes = uModes;
+% % sembem2D.freq = freq;
+% % sembem2D.freqHz = freqHz;
+% % %-----------------
+% % % Post-Processing
+% % %-----------------
+% % % plotModeShapes(sem2D,modeNumPlot);
